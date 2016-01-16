94e1bf0d7a44a87935b1e292b861eeb1eaaf8262
andela-cnwokocha <chidiebere.nwokocha@andela.com>
2016-01-16 03:35:58 +0100

    [Finishes #111680588] Implement the Writer interface and add methods to pass its tests

--- source/checkpoint/andela/db/DbUtils.java
+++ source/checkpoint/andela/db/DbUtils.java
@@ -5,25 +5,37 @@ import java.sql.*;
 import java.util.*;
 
 
-import checkpoint.andela.buffers.*;
 /**
  *
  * About: This interface is implemented by any relational database type. It just holds methods for connecing to a database, and other CRUD functions.
  */
 public interface DbUtils {
 
+  // done
   boolean isDatabaseExist(String databasename) throws SQLException;
 
-  boolean isDatabaseTableExist(String databasename, String tablename) throws SQLException;
+  // done
+  boolean isDatabaseTableExist(String databasename, String tablename);
 
-  boolean createDatabase(String databasename) throws SQLException;
+  //done
+  boolean createDatabase(String databasename) throws SQLException,DbWriterException;
 
+  //done
   boolean deleteDatabase(String databasename) throws SQLException;
 
-  boolean createDatabaseTable(String databasename, String tablename, ArrayList<String> databaseFields) throws SQLException;
+  // done
+  boolean createDatabaseTable(String databasename, String tablename, ArrayList<String> databaseFields) throws SQLException, DbWriterException;
 
-  boolean insertToDatabaseTable(HashedArray bufferedRow, String databasename, String tablename);
+  // done
+  boolean insertToDatabaseTable(HashedArray bufferedRow, String databasename, String tablename) throws SQLException;
 
-  Connection establishConnection();
+  //done
+  Connection connectToDb(String connectionType) throws SQLException;
+
+  //done
+  void registerDriver(String driver);
+
+
+  void closeResources(Statement stmt) throws SQLException;
 
 }
---
+++ source/checkpoint/andela/db/DbWriter.java
@@ -0,0 +1,186 @@
+package checkpoint.andela.db;
+
+import checkpoint.andela.buffers.*;
+import java.sql.*;
+import java.util.*;
+
+/**
+ * Created by chidi on 1/15/16.
+ *
+ *
+ * This class is for connecting a Mysql database, given the connction, and it extending the DbUtils interface. This class therefore carries out connecting to the database, and doing all the CRUD functions on the database given the row that was provided to the appropriate method. This class assumes the user is a root user, with password 1234567890 by default (which can be changed).
+ */
+public class DbWriter implements DbUtils {
+
+  private String dbUrl = "jdbc:mysql://localhost/";
+  private String dbUser = "root";
+  private String dbPassword = "exo14141";
+
+  private String dbname;
+  private Connection connection = null;
+  private String  MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+  private ArrayList<String> availableDatabases = new ArrayList<String>();
+  private ArrayList<String> columnFIelds = new ArrayList<String>();
+
+  public DbWriter(String dbname, ArrayList<String> fieldnames){
+    this.dbname = dbname;
+    this.columnFIelds = fieldnames;
+  }
+
+
+
+  private Connection establishConnection(String connectionDriver) throws SQLException {
+    registerDriver(connectionDriver);
+    this.connection = DriverManager.getConnection(this.dbUrl, this.dbUser, this.dbPassword);
+    return this.connection;
+  }
+
+  @Override
+  public void registerDriver(String driver){
+    try{
+      Class.forName(driver);
+    }catch(ClassNotFoundException cnf){
+      System.err.print(cnf.getMessage());
+      System.exit(1);
+    }
+  }
+
+  @Override
+  public Connection connectToDb(String databasename) throws SQLException{
+    registerDriver(MYSQL_DRIVER);
+    Connection connection = DriverManager.getConnection(this.dbUrl+databasename, this.dbUser, this.dbPassword);
+    return connection;
+  }
+
+  @Override
+  public boolean isDatabaseExist(String dbName) throws SQLException {
+    return getAvailableDatabases().contains(dbName);
+  }
+
+  public ArrayList<String> getAvailableDatabases() throws SQLException{
+    establishConnection(MYSQL_DRIVER);
+    ResultSet databases = this.connection.getMetaData().getCatalogs();
+    while(databases.next()){
+      this.availableDatabases.add(databases.getString(1));
+    }
+
+    return this.availableDatabases;
+  }
+
+  @Override
+  // The database already exists.
+  public boolean isDatabaseTableExist(String dbName, String tableName){
+    boolean availability = false;
+    try{
+      DatabaseMetaData dbMetadata = connectToDb(dbName).getMetaData();
+      ResultSet allTables = dbMetadata.getTables(null,null,tableName,null);
+      availability =  allTables.next();
+    }catch(SQLException sqconnect){
+      System.err.print(sqconnect.getMessage());
+    }
+    return availability;
+  }
+
+  @Override
+  public boolean createDatabase(String databasename) throws SQLException, DbWriterException{
+    int update = 0;
+    if(!isDatabaseExist(databasename)){
+      Statement statement = this.connection.createStatement();
+      update = statement.executeUpdate("Create database "+databasename);
+    }else {
+      throw new DbWriterException("Exception: Database " + databasename + " already exists!");
+    }
+    return update == 1;
+  }
+
+  @Override
+  public boolean deleteDatabase(String databasename) throws SQLException {
+    Statement statement = this.connection.createStatement();
+    return (statement.executeUpdate("Drop Database "+databasename) == 0);
+
+  }
+
+  @Override
+  public boolean createDatabaseTable(String databasename, String tablename, ArrayList<String> tableFields) throws SQLException {
+    setAttributeList(tableFields);
+
+    Statement statement = connectToDb(databasename).createStatement();
+    return (statement.executeUpdate(createTableQuery(tableFields,tablename,"text")) == 0);
+  }
+
+  @Override
+  public boolean insertToDatabaseTable(HashedArray bufferedRow, String databasename, String tablename) throws SQLException {
+
+    int tableColumnSize = tableColumns(tablename) - 1;
+    System.out.println(tableColumnSize);
+    PreparedStatement preparedstatement = this.connection.prepareStatement(insertIntoTableString(tableColumnSize,databasename,tablename));
+    ArrayList<String> allKeys = this.columnFIelds;
+
+    for(int column = 0; column < tableColumnSize; column++){
+      preparedstatement.setString(column+1,inserter(allKeys.get(column),bufferedRow.getBufferRow()));
+    }
+
+    return (preparedstatement.executeUpdate() == 1);
+  }
+
+  @Override
+  public void closeResources(Statement statement) throws SQLException{
+    if(this.connection != null || statement != null){
+      this.connection.close();
+      statement.close();
+    }
+  }
+
+  private String createTableQuery(ArrayList<String> fields, String tablename, String datatype){
+    String query = "Create Table "+tablename+"(id int auto_increment,";
+    for(String field:fields){
+      query+="`"+field+"`"+" "+datatype+",";
+    }
+    return (query.substring(0, query.length() - 1).concat(", primary key(id))"));
+
+  }
+
+  private String insertIntoTableString(int fieldsSize, String databasename, String tablename){
+    String update = "Insert into "+databasename+"."+tablename+" values(default,";
+    for(int field = 0; field < fieldsSize; field++){
+      update+="?,";
+    }
+    return update.substring(0,update.length() - 1).concat(")");
+  }
+
+  private int tableColumns(String tablename) throws SQLException {
+    Integer numberOfColumns = 0;
+
+    DatabaseMetaData databaseMetaData = this.connection.getMetaData();
+    try(ResultSet rs = databaseMetaData.getColumns(null,null,tablename,null)){
+      while(rs.next()){
+        numberOfColumns++;
+      }
+    }
+    return numberOfColumns;
+  }
+
+  private String inserter(String key, HashMap<String,ArrayList<String>> row){
+    String value = null;
+    if(row.containsKey(key)){
+      value = row.get(key).toString();
+    }
+    return value;
+  }
+
+  public void setAttributeList(ArrayList<String> columns){
+   this.columnFIelds = columns;
+  }
+
+  private void setDbUrl(String dburl){
+    this.dbUrl = dburl;
+  }
+
+  private void setDbUser(String dbuser){
+    this.dbUrl = dbuser;
+  }
+
+  private void setDbPassword(String dbPassword){
+    this.dbUrl = dbPassword;
+  }
+}
