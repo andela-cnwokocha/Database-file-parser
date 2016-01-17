9b2b4dcdf1bce572a50b43c531ddbcd62c4225ac
andela-cnwokocha <chidiebere.nwokocha@andela.com>
2016-01-17 11:11:48 +0100

    [Starts #111819053] Added the class from which the program is run, together with its test class

---
+++ source/checkpoint/andela/db/DbWriterThread.java
@@ -0,0 +1,7 @@
+package checkpoint.andela.db;
+
+/**
+ * Created by chidi on 1/17/16.
+ */
+public class DbWriterThread {
+}
---
+++ tests/checkpoint/andela/main/DatFileParserTest.java
@@ -0,0 +1,17 @@
+package checkpoint.andela.main;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by chidi on 1/17/16.
+ */
+public class DatFileParserTest {
+
+  @Test
+  public void testParseFileToDb() throws Exception {
+    DatFileParser datparse = new DatFileParser("Jingoid","jongoic");
+    //datparse.parseToDb();
+
+  }
+}
